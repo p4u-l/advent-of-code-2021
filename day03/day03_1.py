0170da5e54c60fe7da516c72c19afb4d4c42c853
paul-2002 <76221859+paul-2002@users.noreply.github.com>
2021-12-03 11:28:02 +0100

    day03

---
+++ day03/day03_1.py
@@ -0,0 +1,26 @@
+with open("day03/input.txt", "r", encoding="utf-8") as f:
+    diagnostic_report = f.read().splitlines()
+
+
+def most_common_bit_at(bit_str, i):
+    nums_at_i = [num[i] for num in bit_str]
+    ones_at_i = nums_at_i.count("1")
+
+    # Number of 1s greater than or equal to numbers of 0s.
+    if ones_at_i >= (len(nums_at_i) - ones_at_i):
+        return "1"
+    return "0"
+
+
+gamma_rate_str = ""
+bit_length = len(diagnostic_report[0])
+
+for i in range(bit_length):
+    most_common_bit = most_common_bit_at(diagnostic_report, i)
+    gamma_rate_str += most_common_bit
+
+gamma_rate = int(gamma_rate_str, 2)
+# epsilon rate is the gamma rate with inverted bits,
+# formed by bitwise XOR with 1.
+epsilon_rate = gamma_rate ^ int("1" * bit_length, 2)
+print(gamma_rate * epsilon_rate)
---
+++ day03/day03_2.py
@@ -0,0 +1,30 @@
+with open("day03/input.txt", "r", encoding="utf-8") as f:
+    diagnostic_report = f.read().splitlines()
+
+
+def most_common_bit_at(bit_str, i):
+    nums_at_i = [num[i] for num in bit_str]
+    ones_at_i = nums_at_i.count("1")
+
+    # Number of 1s greater than or equal to numbers of 0s.
+    if ones_at_i >= (len(nums_at_i) - ones_at_i):
+        return "1"
+    return "0"
+
+
+oxygen_ratings = diagnostic_report[:]
+co2_ratings = diagnostic_report[:]
+bit_length = len(diagnostic_report[0])
+
+for i in range(bit_length):
+    if len(oxygen_ratings) > 1:
+        most_common_oxygen_bit = most_common_bit_at(oxygen_ratings, i)
+        oxygen_ratings = [r for r in oxygen_ratings if r[i] == most_common_oxygen_bit]
+    
+    if len(co2_ratings) > 1:
+        most_common_co2_bit = most_common_bit_at(co2_ratings, i)
+        co2_ratings = [r for r in co2_ratings if r[i] != most_common_co2_bit]
+
+oxygen_rating = int(oxygen_ratings[0], 2)
+co2_rating = int(co2_ratings[0], 2)
+print(oxygen_rating * co2_rating)
